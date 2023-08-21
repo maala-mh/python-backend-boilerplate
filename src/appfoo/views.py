62aea52fe532ac41f012010b63507e757f80c13a
maala-dxb <maala.m.mhrez@gmail.com>
2023-08-22 00:14:52 +0400

    list gcs files

--- src/appfoo/views.py
+++ src/appfoo/views.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from libfoo import domain
+from libutil import gcs
 from libfoo.models import contracts
 
 
@@ -20,3 +21,8 @@ def get_foo_list():
 @router.post("/foo")
 def add_foo(payload: contracts.AddFoo):
     return domain.foo.add_foo(payload.email)
+
+
+@router.get("/test-gcs")
+def list_files_in_gcs(bucket_name: str):
+    return gcs.list_blobs(bucket_name)
--- src/libfoo/domain/foo.py
+++ src/libfoo/domain/foo.py
@@ -9,13 +9,13 @@ logger = logging.getLogger(__name__)
 
 
 def add_foo(email):
-    sql(engine, """INSERT IGNORE INTO foo (email) VALUES (:email)""", email=email)
+    sql(engine, """INSERT IGNORE INTO foo.foo (email) VALUES (:email)""", email=email)
 
 
 def get_foo_list():
     foo_list = sql(engine, """
         SELECT *
-        FROM foo
+        FROM foo.foo
     """).dicts()
 
     return foo_list
