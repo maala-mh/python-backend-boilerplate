bd21837dc9ffc33c06728defdd986f0909732d77
maala-dxb <maala.m.mhrez@gmail.com>
2023-11-26 15:33:07 +0400

    clean up

--- src/libutil/gcs.py
+++ src/libutil/gcs.py
@@ -8,7 +8,6 @@ logger = logging.getLogger(__name__)
 
 def list_blobs(bucket_name) -> list[str]:
     """Lists all the blobs in the bucket."""
-    logger.info("connecting to project {} bucket_name {}".format(os.environ['GCS_PROJECT'], bucket_name))
     storage_client = storage.Client(project=os.environ['GCS_PROJECT'])
 
     # Note: Client.list_blobs requires at least package version 1.17.0.
